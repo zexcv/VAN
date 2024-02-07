0a9b2b49528717120fa9bb4af0d94b87173045b4
zexcv <krusher13@o2.pl>
2024-02-08 00:56:32 +0100

    Display change

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,14 @@
-import React from "react"
-import { Outlet } from "react-router-dom"
-import Header from "./Header"
-import Footer from "./Footer"
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout() {
-    return (
-        <div className="site-wrapper">
-            <Header />
-            <main>
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="site-wrapper">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
